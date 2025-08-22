8e507286c1355e853550bda0d167a102ebdd9fe7
Christian M. Abuda <chankishiannn@gmail.com>
2025-08-22 09:59:41 +0800

    Update Simple Expenses Tracker.py

--- Simple Expenses Tracker File Handling Implementation/Simple Expenses Tracker.py
+++ Simple Expenses Tracker File Handling Implementation/Simple Expenses Tracker.py
@@ -54,3 +54,30 @@ def edit_Income():
     else:
         print("No similar income found! ")
     
+userChoose = 0
+while userChoose != 5:
+    print("\n[1] Add Income")
+    print("[2] Delete Income")
+    print("[3] Edit Income")
+    print("[4] View Income")
+    print("[5] Exit")
+
+    try:
+        userChoose = int(input("Please Choose from the option: "))
+    except errorMessage:
+        print("Invalid Input! Please Try Again: ")
+        continue
+
+    if useChoose == 1:
+        add_Income()
+    elif useChoose == 2:
+        delete_Income()
+    elif useChoose == 3:
+        edit_Income()
+    elif useChoose == 4:
+        view_Income()
+    elif useChoose == 5:
+        print("Exiting program! Thank youuu and Goodbye....")
+    else:
+        print("Invalid Input! PLease Try Again")
+        
